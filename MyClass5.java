e204383393e6c5975d8489d1d92e9635b5046a60
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-10-30 17:36:31 +0000

    P5 Commit

---
+++ MyClass5.java
@@ -0,0 +1,12 @@
+package mypackage5;
+
+
+
+public class MyClass5
+{
+    public static void main(String[] args)
+
+    {
+        System.out.println("What are the Stages of SDLC and STLC?");
+    }
+}
